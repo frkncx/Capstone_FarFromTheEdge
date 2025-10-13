dcc9d1bb27f958ea1020b8b8e7181a4e2045b989
PedroMeyer02 <pedromeyer02ca@gmail.com>
2025-10-13 14:28:48 -0400

    Dialogue
    Added a Player Layer;
    Added two scripts, one for the dialogue itself and other to trigger the dialogues.
    
    Feel free to change as it feels better for you.

---
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class DialogueController : MonoBehaviour
+{
+    [Header("Control Settings")]
+    public string dialogueName = "";
+    public bool isDialogueOver = false;
+
+
+    [Header("Dialogue Settings")]
+    public TextMeshProUGUI textComponent;
+    public string[] dialogueText; //previous lines
+    public float textSpeed;
+    private int index;
+
+    private void OnEnable() //The video references this as Start, not OnEnable
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //Button isn't working
+        {
+            if (textComponent.text == dialogueText[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = dialogueText[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in dialogueText[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < dialogueText.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine (TypeLine());
+        }
+        else
+        {
+            isDialogueOver = true; //I added this line
+            gameObject.SetActive(false);
+            this.enabled = false; //I added this line
+        }
+    }
+}
